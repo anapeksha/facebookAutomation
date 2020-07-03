a41c8ddffbed7a54b695d06994b0c775bf22ccdf
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2020-07-03 22:26:14 +0530

    Created python Script!

---
+++ automated_fb_messenger.py
@@ -0,0 +1,33 @@
+import fbchat
+import json
+from getpass import getpass
+
+
+cookies = {}
+try:
+    # Load the session cookies
+    with open('session.json', 'r') as f:
+        cookies = json.load(f)
+except:
+    # If it fails, never mind, we'll just login again
+    pass
+
+username = input("Username:- ")
+client = fbchat.Client("username", getpass, session_cookies=cookies)
+
+# Client work
+name = input("Search for a friend: ")
+friends = client.searchForUsers(name)
+friend = friends[0]
+msg = input("Type the message: ")
+sent = client.sendMessage(msg, thread_id=friend.uid)
+if sent:
+    print("Message sent successfully!!")
+
+# Saving session cookies
+
+with open('session.json', 'w') as f:
+    json.dump(client.getSession(), f)
+
+
+       
